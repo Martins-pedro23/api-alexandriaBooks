c929b0af35e7130993a156831bcac651006955af
Kyoudan <fogoste@gmail.com>
2023-06-25 11:36:12 -0300

    feat: adding cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
-import "./database/connection"
-import express from "express"
-import routes from "./routes/index"
+import "./database/connection";
+import express from "express";
+import routes from "./routes/index";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(express.json(), express.urlencoded({extended: true}))
-app.use(routes)
+app.use(cors());
+app.use(express.json(), express.urlencoded({ extended: true }));
+app.use(routes);
 
-export{app} 
+export { app };
